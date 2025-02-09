5e0da3c6b8fc77eb17faaca79122093ab22038ba
ZaireTheGreatt <zairevilla1@gmail.com>
2025-02-10 04:19:13 +0800

    change header contents

--- src/components/forms/firstStepForm.tsx
+++ src/components/forms/firstStepForm.tsx
@@ -1,7 +1,7 @@
 export const FirstStepForm = () => {
   return (
     <section>
-      <h1 className="text-3xl text-accent font-bold">Be my valentines?</h1>
+      <h1 className="text-3xl text-accent font-bold">Are you Jeanine Gabrielle Cruz?</h1>
     </section>
   );
 };
